9bdfb31a1d1956aa2459a97056e115e36222d2f1
Axel Da Silva <axelibnida@gmail.com>
2023-05-24 13:16:52 +0200

    redo all

--- app.py
+++ app.py
@@ -1,51 +1,42 @@
 from flask import Flask, request, jsonify
-import pandas as pd
 import gensim
-import pickle
-import nltk
+from gensim import corpora
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-
-nltk.download('punkt')
 
 app = Flask(__name__)
 
-# Load the data and preprocess
-data = pd.read_csv('your_data.csv')
+# Load the saved model and dictionary
+lda_model = gensim.models.LdaModel.load('lda_model')
+dictionary = gensim.corpora.Dictionary.load('dictionary.gensim')
+
+stop_words = set(stopwords.words('english'))
 
 def preprocess(text):
-    stop_words = set(stopwords.words('english'))
-    lemmatizer = WordNetLemmatizer()
-    tokenized_text = word_tokenize(text.lower())
-    cleaned_text = [lemmatizer.lemmatize(t) for t in tokenized_text if t not in stop_words and t.isalpha()]
-    return cleaned_text
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3 and token not in stop_words:
+            result.append(token)
+    return result
 
-data['tokens'] = data['text'].apply(preprocess)
-data['tags'] = data['tags'].str.split(',')
+def predict_topic(text):
+    # Preprocess the input text
+    processed_text = preprocess(text)
 
-# Load the LDA model and dictionary
-with open('lda_model.pkl', 'rb') as f:
-    lda_model = pickle.load(f)
+    # Convert text to bag-of-words format
+    bow_text = dictionary.doc2bow(processed_text)
 
-with open('dictionary.pkl', 'rb') as f:
-    dictionary = pickle.load(f)
+    # Get the topic distribution of the text
+    topic_distribution = lda_model.get_document_topics(bow_text)
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Welcome to my LDA model API. Use /predict to access the model."
+    return topic_distribution
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        data = request.json
-        text = data['text']
-        processed_text = preprocess(text)
-        bow_vector = dictionary.doc2bow(processed_text)
-        lda_vector = lda_model[bow_vector]
-        sorted_topics = sorted(lda_vector, key=lambda x: x[1], reverse=True)
-        tags = [dictionary[id] for id, _ in sorted_topics[:2]]
-        return jsonify(tags)
+    data = request.json
+    text = data['text']
+    result = predict_topic(text)
+    return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run()
