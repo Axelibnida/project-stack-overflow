71b236e546195b0b54d7f20dec23fb1a4879239c
Axel Da Silva <axelibnida@gmail.com>
2023-05-24 11:57:57 +0200

    update 24-05-23

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import gensim
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Load the LDA model and dictionary
+with open('lda_model.pkl', 'rb') as f:
+    lda_model = pickle.load(f)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+with open('dictionary.pkl', 'rb') as f:
+    dictionary = pickle.load(f)
+
+# Preprocessing function (assuming the same preprocessing as during model training)
+def preprocess(text):
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    tokenized_text = word_tokenize(text.lower())
+    cleaned_text = [lemmatizer.lemmatize(t) for t in tokenized_text if t not in stop_words and t.isalpha()]
+    return cleaned_text
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data = request.json
+        text = data['text']
+        processed_text = preprocess(text)
+        bow_vector = dictionary.doc2bow(processed_text)
+        lda_vector = lda_model[bow_vector]
+        # sort topics by their relevance
+        sorted_topics = sorted(lda_vector, key=lambda x: x[1], reverse=True)
+        # get the top 2 topics as tags
+        tags = [dictionary[id] for id, _ in sorted_topics[:2]]
+        return jsonify(tags)
+
+if __name__ == '__main__':
+    app.run(port=5000)
