69e1cb1c21c3635646185be3788b469c8a3475ce
Axel Da Silva <axelibnida@gmail.com>
2023-05-30 20:37:42 +0200

    poom poof

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import pickle
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -26,48 +31,57 @@ topic_tags = {
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Load the models, dictionary, vectorizer and label binarizer
-    with open('lda_model.pkl', 'rb') as f:
-        lda_model = pickle.load(f)
-    with open('rf_model.pkl', 'rb') as f:
-        rf_model = pickle.load(f)
-    with open('dictionary.pkl', 'rb') as f:
-        dictionary = pickle.load(f)
-    with open('vectorizer.pkl', 'rb') as f:
-        vectorizer = pickle.load(f)
-    with open('mlb.pkl', 'rb') as f:
-        mlb = pickle.load(f)
+    try:
+        logger.info('Loading the models and dictionary...')
+        # Load the LDA model, RF model, dictionary, vectorizer and label binarizer
+        with open('lda_model.pkl', 'rb') as f:
+            lda_model = pickle.load(f)
+        with open('rf_model.pkl', 'rb') as f:
+            rf_model = pickle.load(f)
+        with open('dictionary.pkl', 'rb') as f:
+            dictionary = pickle.load(f)
+        with open('vectorizer.pkl', 'rb') as f:
+            vectorizer = pickle.load(f)
+        with open('mlb.pkl', 'rb') as f:
+            mlb = pickle.load(f)
+        logger.info('Models and dictionary loaded successfully.')
 
-    # Get the input text from the request
-    data = request.get_json()
-    text = data['text']
+        # Get the input text from the request
+        logger.info('Processing the request data...')
+        data = request.get_json()
+        text = data['text']
 
-    # Preprocess the input text
-    tokens = word_tokenize(text.lower())
-    tokens = [token for token in tokens if token.isalpha()]
-    tokens = [token for token in tokens if token not in stopwords.words('english')]
+        # Preprocess the input text
+        tokens = word_tokenize(text.lower())
+        tokens = [token for token in tokens if token.isalpha()]
+        tokens = [token for token in tokens if token not in stopwords.words('english')]
 
-    # Create a bag-of-words representation of the input text
-    bow = dictionary.doc2bow(tokens)
-    vec = vectorizer.transform([' '.join(tokens)])
+        # Create a bag-of-words representation of the input text
+        bow = dictionary.doc2bow(tokens)
 
-    # Perform topic inference and tag prediction
-    lda_topics = lda_model[bow]
-    rf_prediction = rf_model.predict(vec)
-    tags = mlb.inverse_transform(rf_prediction)
+        # Perform LDA topic inference
+        lda_topics = lda_model[bow]
 
-    # Convert topics and tags to a JSON serializable format
-    topics_serializable = [
-        {'topic': topic_tags[topic[0]], 'probability': float(topic[1])}
-        for topic in lda_topics
-    ]
-    tags_serializable = list(tags[0])
+        # Perform RF tag prediction
+        X = vectorizer.transform([' '.join(tokens)])
+        y = rf_model.predict(X)
+        tags = mlb.inverse_transform(y)
 
-    # Return the inferred topics and predicted tags
-    result = {'topics': topics_serializable, 'tags': tags_serializable}
-    return jsonify(result)
+        # Convert topics to a JSON serializable format
+        lda_topics_serializable = [
+            {'topic': topic_tags[topic[0]], 'probability': float(topic[1])}
+            for topic in lda_topics
+        ]
+        tags_serializable = list(tags[0])
 
+        # Return the inferred topics and predicted tags
+        result = {'topics': lda_topics_serializable, 'tags': tags_serializable}
+        logger.info('Inference complete. Sending response...')
+        return jsonify(result)
 
+    except Exception as e:
+        logger.error('An error occurred: ' + str(e))
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run()
